1907062a615877f2d2f9c5348d67ed723b22ba64
Alex Florisca <alexflorisca@gmail.com>
2019-10-19 13:22:55 +0100

    Set port from env

--- server.js
+++ server.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const app = express();
-const PORT = 3000;
+const DEFAULT_PORT = 3000;
 
 app.get('/', (req, res) => { res.send("Server up and running"); });
 
-app.listen(PORT, () => { console.log(`Listening on port ${PORT}`)});
+app.listen(process.env.PORT || DEFAULT_PORT, () => { 
+	console.log(`Listening on port ${DEFAULT_PORT}`)
+});
 
