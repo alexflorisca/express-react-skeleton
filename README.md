c295c1ffeef10ac6c2fe1a7b2137e6c6c4bcad2b
Alex Florisca <alexflorisca@gmail.com>
2019-10-23 03:27:22 +0100

    README

---
+++ README.md
@@ -0,0 +1,3 @@
+# Express React Skeleton
+
+A minimal server side rendered app skeleton using express, react and sensible, simple tools.
--- server/ssr.js
+++ server/ssr.js
@@ -48,7 +48,6 @@ function insertAppMarkup(html, appMarkup) {
 		return `${startingHtml}${appMarkup}${endHtml}`;
 }
 
-const lift2 = f => g => h => x => f(g(x))(h(x));
 
 /**
  * Render a page on request
